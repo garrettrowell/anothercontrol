5959409a4b7932afb84814cfae000e6a49fadaef
Garrett Rowell <garrett@puppet.com>
2022-09-08 13:41:02 -0400

    try

--- site-modules/adhoc/plans/myplan.pp
+++ site-modules/adhoc/plans/myplan.pp
@@ -4,12 +4,21 @@ plan adhoc::myplan(
   TargetSpec $targets,
 ) {
 
-    $backup_result_or_error = run_task('exec', $targets, 'command' => 'whoami', '_catch_errors' => true)
-    $backup_result = case $backup_result_or_error {
-      ResultSet: { $backup_result_or_error } # this is when successful
-      Error['bolt/run-failure']: { $backup_result_or_error.details['result_set'] } # if an error extract its result set
-      default: { fail_plan($backup_result_or_error) } # handle unexpected errors
-    }
-    run_task('exec', get_targets($backup_result.ok_set), 'command' => 'echo hello')
-
+  #    $backup_result_or_error = run_task('exec', $targets, 'command' => 'whoami', '_catch_errors' => true)
+  #    $backup_result = case $backup_result_or_error {
+  #      ResultSet: { $backup_result_or_error } # this is when successful
+  #      Error['bolt/run-failure']: { $backup_result_or_error.details['result_set'] } # if an error extract its result set
+  #      default: { fail_plan($backup_result_or_error) } # handle unexpected errors
+  #    }
+  #    run_task('exec', get_targets($backup_result.ok_set), 'command' => 'echo hello')
+  $result_or_error = run_task('exec', 'command' => 'whoami', '_catch_errors' => true)
+  $result = case $result_or_error {
+    # When the plan returned a ResultSet use it.
+    ResultSet: { $result_or_error }
+    # If the run_task failed extract the result set from the error.
+    Error['bolt/run-failure'] : { $result_or_error.details['result_set'] }
+    # The sub-plan failed for an unexpected reason.
+    default : { fail_plan($result_or_error) } }
+  # Run a task on the successful targets
+  run_task('exec', $result.ok_set, 'command' => 'echo hello')
 }
