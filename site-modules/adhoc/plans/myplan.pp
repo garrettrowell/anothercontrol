23d29226a68f37887c56221dfde652ff7e62d5b2
Garrett Rowell <garrett@puppet.com>
2022-09-08 13:28:55 -0400

    ugh

--- site-modules/adhoc/plans/myplan.pp
+++ site-modules/adhoc/plans/myplan.pp
@@ -4,6 +4,12 @@ plan adhoc::myplan(
   TargetSpec $targets,
 ) {
 
-  # plan steps go here
+    $backup_result_or_error = run_task('exec', $target, 'command' => 'whoami', '_catch_errors' => true)
+    $backup_result = case $backup_result_or_error {
+      ResultSet: { $backup_result_or_error } # this is when successful
+      Error['bolt/run-failure']: { $backup_result_or_error.details['result_set'] } # if an error extract its result set
+      default: { fail_plan($backup_result_or_error) } # handle unexpected errors
+    }
+    run_task('exec', $backup_result.ok_set, 'command' => 'echo hello')
 
 }
