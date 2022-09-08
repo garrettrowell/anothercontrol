d5c5a58be2eec00b0c71c0b707a5ef1ec4b0a7ef
Garrett Rowell <garrett@puppet.com>
2022-09-08 14:37:26 -0400

    test

--- site-modules/adhoc/plans/myplan.pp
+++ site-modules/adhoc/plans/myplan.pp
@@ -12,12 +12,12 @@ plan adhoc::myplan(
   #    }
   #    run_task('exec', get_targets($backup_result.ok_set), 'command' => 'echo hello')
 
-  $result_or_error = run_task('exec', $targets, 'command' => 'false', '_catch_errors' => true)
+  $result_or_error = run_task('exec', $targets, 'command' => 'curl https://somecrudurl', '_catch_errors' => true)
   $result = case $result_or_error {
     # When the plan returned a ResultSet use it.
     ResultSet: { $result_or_error }
     # If the run_task failed extract the result set from the error.
-    #Error['bolt/run-failure'] : { $result_or_error.details['result_set'] }
+    Error['bolt/run-failure'] : { $result_or_error.details['result_set'] }
     # The sub-plan failed for an unexpected reason.
     default : { fail_plan($result_or_error) } }
 
