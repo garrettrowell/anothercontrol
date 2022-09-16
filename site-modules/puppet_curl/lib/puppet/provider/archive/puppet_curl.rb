66a42020a4051be06703eddb1a5387307f605614
Garrett Rowell <garrett@puppet.com>
2022-09-16 17:53:43 -0400

    fix n remove

--- site-modules/puppet_curl/lib/puppet/provider/archive/puppet_curl.rb
+++ site-modules/puppet_curl/lib/puppet/provider/archive/puppet_curl.rb
@@ -1,6 +1,4 @@
-require 'uri'
-require 'tempfile'
 
 Puppet::Type.type(:archive).provide(:puppet_curl, parent: :curl) do
-  commands curl: '/opt/puppetlabs/puppet/bin/noexist'
+  commands curl: '/opt/puppetlabs/puppet/bin/curl'
 end
