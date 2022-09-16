b194124d65bc56d33778987f4a6abc271122bdb7
Garrett Rowell <garrett@puppet.com>
2022-09-16 17:51:26 -0400

    break things

--- site-modules/puppet_curl/lib/puppet/provider/archive/puppet_curl.rb
+++ site-modules/puppet_curl/lib/puppet/provider/archive/puppet_curl.rb
@@ -2,5 +2,5 @@ require 'uri'
 require 'tempfile'
 
 Puppet::Type.type(:archive).provide(:puppet_curl, parent: :curl) do
-  commands curl: '/opt/puppetlabs/puppet/bin/curl'
+  commands curl: '/opt/puppetlabs/puppet/bin/noexist'
 end
