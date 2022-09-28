67ba5e3c9df1766ff2548d515e80eed9cddf7156
Garrett Rowell <garrett@puppet.com>
2022-09-28 17:06:46 -0400

    debug

--- site-modules/test_nagios/manifests/init.pp
+++ site-modules/test_nagios/manifests/init.pp
@@ -2,28 +2,27 @@ class test_nagios () {
   # tmp should be removed from path once verified everything works
   $nagios_cfg_base_path = '/tmp/omd/sites/ops/etc/nagios/conf.d'
 
-  $customer = 'a_cust'
-
   # lint:ignore:140chars
   $nagios_cfg_path = $customer ? {
-    undef   => "${nagios_cfg_base_path}/${facts['networking']['hostname']}.cfg",
-    default => "${nagios_cfg_base_path}/cust/${customer}/${facts['networking']['hostname']}.cfg",
+    undef   => "${nagios_cfg_base_path}/${facts['org']['env']}/${facts['org']['country']}/${facts['networking']['hostname']}.cfg",
+    default => "${nagios_cfg_base_path}/cust/${facts['org']['customer']}/${facts['org']['country']}/${facts['networking']['hostname']}.cfg",
   }
   # lint:endignore
 
   # need to split the path so that we can rebuild it piece by piece
   $nagios_cfg_path_elms = split($nagios_cfg_path, '/')
-
-  # /tmp/omd/sites/ops/etc/nagios/conf.d/cust
-  # export the full directory structure
+  #
+  #  # /tmp/omd/sites/ops/etc/nagios/conf.d/cust
+  #  # export the full directory structure
   $nagios_cfg_path_elms.each |$index, $value| {
     unless $index <= 1 {
       $new_path = join($nagios_cfg_path_elms[0,$index], '/')
-      @@file { "${trusted['certname']} - ${new_path}":
-        path   => $new_path,
-        ensure => directory,
-        tag    => 'nagios_cfg_path',
-      }
+      echo { "new_path = ${new_path}": }
+  #      @@file { "${trusted['certname']} - ${new_path}":
+  #        path   => $new_path,
+  #        ensure => directory,
+  #        tag    => 'nagios_cfg_path',
+  #      }
     }
   }
 
