706a4badd4213bf4e6001130b6f7ddfe574a0a1b
Garrett Rowell <garrett@puppet.com>
2022-09-28 16:01:43 -0400

    foo

--- site-modules/test_nagios/manifests/init.pp
+++ site-modules/test_nagios/manifests/init.pp
@@ -2,9 +2,9 @@ class test_nagios () {
   # tmp should be removed from path once verified everything works
   $nagios_cfg_base_path = '/tmp/omd/sites/ops/etc/nagios/conf.d'
 
-  # lint:ignore:140chars
   $customer = 'a_cust'
 
+  # lint:ignore:140chars
   $nagios_cfg_path = $customer ? {
     undef   => "${nagios_cfg_base_path}/${facts['networking']['hostname']}.cfg",
     default => "${nagios_cfg_base_path}/cust/${customer}/${facts['networking']['hostname']}.cfg",
@@ -14,11 +14,13 @@ class test_nagios () {
   # need to split the path so that we can rebuild it piece by piece
   $nagios_cfg_path_elms = split($nagios_cfg_path, '/')
 
+  # /tmp/omd/sites/ops/etc/nagios/conf.d/cust
   # export the full directory structure
   $nagios_cfg_path_elms.each |$index, $value| {
     unless $index <= 1 {
       $new_path = join($nagios_cfg_path_elms[0,$index], '/')
-      @@file { $new_path:
+      @@file { "${trusted['certname']} - ${new_path}":
+        path   => $new_path,
         ensure => directory,
         tag    => 'nagios_cfg_path',
       }
