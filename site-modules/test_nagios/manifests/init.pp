9d26888ce2a23be778fbf3700042cc5d4d273a6b
Garrett Rowell <garrett@puppet.com>
2022-09-29 09:55:59 -0400

    asdf

--- site-modules/test_nagios/manifests/init.pp
+++ site-modules/test_nagios/manifests/init.pp
@@ -46,7 +46,7 @@ class test_nagios () {
     # Only concerned with the base path for now
     $cfg_elms = split($nagios_cfg_base_path, '/')
     $cfg_elms.each |$index, $value| {
-      unless $index <= 1 {
+      unless $index < 1 {
         $n_p1 = join($cfg_elms[0, $index], '/')
         echo { "n_p1 = ${n_p1}": }
       }
